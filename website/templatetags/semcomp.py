9a348d6a047fa737b676eff80b4c65a9b2deafd4
Felipe Bessa Coelho <fcoelho.9@gmail.com>
2014-01-04 17:38:49 -0200

    tag render_user_bar
    uma tag usada pra renderizar uma barra com
    informações básicas pro usuário, como o
    nome dele e um link de logout

--- website/templatetags/semcomp.py
+++ website/templatetags/semcomp.py
@@ -10,3 +10,11 @@ def render_sponsors():
 		'sponsors': Company.objects.filter(type='P'),
 		'partners': Company.objects.filter(type='A'),
 	}
+
+@register.inclusion_tag('website/templatetags/render_user_bar.html', takes_context=True)
+def render_user_bar(context):
+	user = context['user']
+	return {
+		 'user': user,
+		 'user_name': user.get_full_name(),
+	}
