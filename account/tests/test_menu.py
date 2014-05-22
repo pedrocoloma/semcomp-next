364de52b29faba9d4dff4bc6ae67a5b3dcbad937
Felipe Bessa Coelho <fcoelho.9@gmail.com>
2014-05-21 23:37:20 -0300

    Adiciona testes pro menu do usuário
    Os testes verificam se o menu de usuário correto é mostrado quando tem
    usuário logado e quando não tem

---
+++ account/tests/test_menu.py
@@ -0,0 +1,57 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.auth.tests.utils import skipIfCustomUser
+from django.core.urlresolvers import reverse
+from django.template import Template, Context, RequestContext
+from django.test import TestCase
+from django.test.client import RequestFactory
+from django.utils import six
+
+from menus.menu_pool import menu_pool
+
+class UserMenuTests(TestCase):
+	def setUp(self):
+		self.factory = RequestFactory()
+		self.anon_user = AnonymousUser()
+		self.default_user = User.objects.create_user(
+			'user',
+			'email@provider.com',
+			'pass',
+			first_name='John',
+			last_name='Doe',
+		)
+
+	def get_menu_titles(self, user):
+		# o RequestFactory é um troço bem simples que não lida com sessões ou
+		# autenticação. Precisa colocar o atributo do usuário na mão
+		request = self.factory.get('/')
+		request.user = user
+
+		# Isso aqui é uma lista (gênio)
+		menu_nodes = menu_pool.get_nodes(request)
+		# a função get_menu_title retorna um objeto do tipo
+		# django.utils.functional.__proxy__, que nada mais é do que uma string
+		# que foi passada pro gettext como _('string em português'). Pra ela
+		# virar uma "string de verdade", precisa chamar unicode(proxy). Mas
+		# como em python 3 não existe o tipo "unicode", eu uso six.text_type,
+		# que é str em py3 ou unicode em py2 e fica tudo certo
+		menu_title_proxies = [n.get_menu_title() for n in menu_nodes]
+		menu_titles = map(six.text_type, menu_title_proxies)
+
+		return menu_titles
+
+	def test_user_logged_in_menu(self):
+		menu_titles = self.get_menu_titles(self.default_user)
+
+		self.assertIn('Área de usuário', menu_titles)
+		self.assertNotIn('Entrar', menu_titles)
+
+	def test_user_not_logged_in_menu(self):
+		menu_titles = self.get_menu_titles(self.anon_user)
+
+		self.assertNotIn('Área de usuário', menu_titles)
+		self.assertIn('Entrar', menu_titles)
+
