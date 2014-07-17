b7aacd4311a8dd6e145efbb5a51a29aab93338c9
Bruno Orlandi <brorlandi@gmail.com>
2014-07-17 01:19:58 -0300

    Correção, agora troca a URL do minicurso.

--- website/static/js/app.js
+++ website/static/js/app.js
@@ -3,10 +3,10 @@
 $(document).foundation();
 
 $('a[data-reveal-id="modal-reveal"]').each(function(){
-    console.log($(this));
+    //console.log($(this).attr("href"));
     var link = $(this);
     $(this).click(function(){
-        console.log($(this).attr("href"));
+        //console.log(">"+$(this).attr("href"));
         var url = $(this).attr("href");
         window.history.pushState("A semcomp é foda!","Semcomp 17",url);
     });
@@ -16,6 +16,15 @@ $('a[data-reveal-id="modal-reveal"]').each(function(){
 var trigger = 0; //  Serve para guardar o estado e se estiver fechando, volta no histórico mas nao tenta fechar de novo. E se estiver voltando no histórico, fecha o modal e não tenta voltar de novo.
 
 $(document).on('close', '#modal-reveal', function () {
+    back_modal();
+});
+
+$(document).on('opened', '#modal-reveal', function () {
+    course_modal();
+});
+
+function back_modal(){
+    //console.log("back_modal");
     if(trigger == 0){
         trigger = 1;
         history.back();
@@ -23,14 +32,33 @@ $(document).on('close', '#modal-reveal', function () {
     else{
         trigger = 0;
     }
-});
+}
 
 window.onpopstate = function(event) {
+    //console.log("popstate");
     if(trigger == 1){
         trigger = 0;
     }
     else{
         trigger = 1;
         $("#modal-reveal").foundation('reveal', 'close');
+        //$("#course-modal").foundation('reveal', 'close');
     }
 };
+
+function course_modal(){
+    //console.log("course_modal");
+
+    $('a[data-reveal-id="course-modal"]').each(function(){
+        //console.log($(this).attr("href"));
+        var link = $(this);
+        $(this).click(function(){
+            //console.log(">>>"+$(this).attr("href"));
+            var url = $(this).attr("href");
+            window.history.replaceState("A semcomp é foda!","Semcomp 17",url);
+        });
+    });
+    $(document).on('close', '#course-modal', function () {
+        back_modal();
+    });
+}
