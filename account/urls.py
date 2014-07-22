24b0efa1f5feef650fd0f7b3205171e4a06e1c28
André <andre.missaglia@gmail.com>
2014-07-21 23:34:55 -0300

    Adiciona tela de envio de comprovantes.

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import patterns, url, include
 urlpatterns = patterns('',
 	url(r'^$', 'account.views.account_overview', name='account_overview'),
 	url(r'^pagamento/$', 'account.views.payment_overview', name='account_payment'),
+	url(r'^pagamento/send/$', 'account.views.payment_send', name='account_payment_send'),
 	url(r'^minicursos/$', 'account.views.courses', name='account_courses'),
 )
