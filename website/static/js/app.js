ad609ad6f464b1fe48a3b4e04fd030e800911d66
Bruno Orlandi <brorlandi@gmail.com>
2014-07-23 04:02:00 -0300

    As URL funcionam perfeitamente para cliques do mouse, porem para os botões de Voltar e Avançar dá vários problemas. Alterações por @fcoelho.

--- website/static/js/app.js
+++ website/static/js/app.js
@@ -2,31 +2,76 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
-$('a[data-reveal-id="modal-reveal"]').each(function(){
-    var link = $(this);
-    $(this).click(function(){
-        //console.log(">"+$(this).attr("href"));
-        var url = $(this).attr("href");
-        window.history.pushState("A semcomp é foda!","Semcomp 17",url);
-    });
-});
-setConfigCourseModal();
+(function ($, window, document) {
+    var ScheduleData = {
+        anchorURL: null,
+        modal: null,
+        baseURL: location.pathname,
+        triggeredByPop: false,
+    };
 
-$(document).on('opened', '#modal-reveal', function () {
-    setConfigCourseModal();
-});
-
-function setConfigCourseModal(){
-    $('a[data-reveal-id="course-modal"]').each(function(){
-        var link = $(this);
-        $(this).click(function(){
-            //console.log(">>"+$(this).attr("href"));
-            var url = $(this).attr("href");
-            window.history.replaceState("A semcomp é foda!","Semcomp 17",url);
+    function configModals() {
+        $('a[data-reveal-id]')
+            .off('click.semcomp')
+            .on('click.semcomp', function() {
+                var anchor = $(this);
+                ScheduleData.anchorURL = anchor.attr('href');
+                ScheduleData.modal = getOpenModal();
         });
+    }
+
+    function getOpenModal() {
+        var selected = $('.reveal-modal.open[data-reveal]');
+        if (selected.length == 1)
+            return selected.attr('id');
+        else
+            return null;
+    }
+
+    configModals();
+
+    $(document).on('opened.fndtn.reveal', '[data-reveal]', function(e) {
+        configModals();
+
+        if (ScheduleData.triggeredByPop) {
+            ScheduleData.triggeredByPop = false;
+        } else {
+            window.history.pushState(
+                {
+                    modalID: $(this).attr('id'),
+                }, null,
+                ScheduleData.anchorURL
+            );
+        }
     });
-}
-window.onpopstate = function(event) {
-    $("#modal-reveal").foundation('reveal', 'close');
-    $("#course-modal").foundation('reveal', 'close');
-};
+
+    $(document).on('close.fndtn.reveal', '[data-reveal]', function(e) {
+        configModals();
+
+        if (ScheduleData.triggeredByPop) {
+            ScheduleData.triggeredByPop = false;
+        } else {
+            window.history.pushState(
+                {
+                    modalID: $(this).attr('id'),
+                }, null,
+                ScheduleData.baseURL
+            );
+        }
+    });
+
+    window.onpopstate = function(e) {
+        configModals();
+        ScheduleData.triggeredByPop = true;
+
+        if (e.state && e.state.modalID) {
+            ScheduleData.anchorURL = location.pathname;
+            $('#' + e.state.modalID).foundation('reveal', 'open', location.pathname);
+        } else {
+            var modal = getOpenModal();
+            if (modal) {
+                $('#' + modal).foundation('reveal', 'close')
+            }
+        }
+    };
+}(jQuery, window, window.document));
