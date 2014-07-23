2c3cd7f7acf32572488ad35cfb1814c91bad2b73
André <andre.missaglia@gmail.com>
2014-07-23 00:41:11 -0300

    Cria página de logout, resolve #10

--- account/urls.py
+++ account/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
 	url(r'^$', 'account.views.account_overview', name='account_overview'),
 	url(r'^pagamento/$', 'account.views.payment', name='account_payment'),
 	url(r'^minicursos/$', 'account.views.courses', name='account_courses'),
+	url(r'^logout/$', 'account.views.account_logout', name='account_logout'),
 )
--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.contrib.auth import logout
 
 @login_required
 def account_overview(request):
@@ -12,3 +13,7 @@ def payment(request):
 @login_required
 def courses(request):
 	return render(request, 'account/courses.html', {'active_courses': True})
+
+def account_logout(request):
+	logout(request)
+	return render(request, 'account/logout.html')
