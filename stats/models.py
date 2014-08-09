b44c11e45150d7f35cf9f2b5dcbc29b787c8ac46
Felipe Bessa Coelho <fcoelho.9@gmail.com>
2014-08-09 05:49:24 -0300

    Coleta estatísticas de cadastro de usuários

--- stats/models.py
+++ stats/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+from django.dispatch import receiver
 
-# Create your models here.
+from signup.signals import user_signed_up, user_activated
+
+from stats import add_event
+
+
+@receiver(user_signed_up)
+def user_signed_up_callback(sender, **kwargs):
+	user = kwargs.pop('user')
+
+	add_event(
+		'signup-users',
+		{
+			'action': 'signup',
+			'user': {
+				'id': user.pk,
+				'name': user.full_name,
+			},
+		}
+	)
+
+@receiver(user_activated)
+def user_activated_callback(sender, **kwargs):
+	user = kwargs.pop('user')
+
+	add_event(
+		'signup-users',
+		{
+			'action': 'activate',
+			'user': {
+				'id': user.pk,
+				'name': user.full_name,
+			}
+		}
+	)
+
