3fb5c6c7aceea9ec301e0b193b176eb1bd632b49
Bruno Orlandi <brorlandi@gmail.com>
2014-08-17 00:30:54 -0300

    Coleta minicurso que foi enviado a mensagem nas estatisticas.

--- management/views/messages.py
+++ management/views/messages.py
@@ -157,6 +157,7 @@ def messages_new(request):
 			# determina pra quem vai a mensagem
 			to_type = form.cleaned_data['type']
 			to_user = form.cleaned_data['to_email']
+			to_course = form.cleaned_data['to_course']
 			if to_type == 'one':
 				users = [form.cleaned_data['to_email']]
 			elif to_type == 'bulk':
@@ -187,6 +188,8 @@ def messages_new(request):
 			}
 			if to_type == 'one':
 				stats_data['message']['recipient'] = to_user.email
+			elif to_type == 'course':
+				stats_data['course'] = to_course.title
 			stats.add_event('management-messages', stats_data)
 
 			for user in users:
